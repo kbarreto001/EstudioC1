792b3369fd66241c3f07242fe39d263af0a98d39
kbarreto001 <kbarreto001@ubuntu>
2019-01-25 16:06:25 +0100

    Iniciando estudio de C

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=estudioc1
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/estudioc1
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=estudioc1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/estudioc1.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=estudioc1
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/estudioc1
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=estudioc1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/estudioc1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
